e0ae0818c7c717ccfa19723e607d634f0de2fb71
kevm <scoundrel@gmail.com>
2012-02-17 15:55:25 -0600

    common assembly info got messed up

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("5e437d360470e654008af1800d0e29fdf6a498de")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
+
